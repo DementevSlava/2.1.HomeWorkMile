17026b7d07340a42de21f4d0529b52a3874deeb3
Dementev Slava <kursant344@gmail.com>
2020-03-26 13:20:20 +0300

    first commit

---
+++ Mile.java
@@ -0,0 +1,10 @@
+//package Blok_3_TipyDannyh.Z1_Mile; //Заменить на свой (Для IDEA)
+
+public class Mile {
+    public static void main(String[] args) {
+        int tickePprice = 10000;
+        int mile = tickePprice / 20; //расчет колово милей
+        System.out.println("Вам начислено: " + mile + " милей");
+        System.out.println("Спасибо, что воспользовалиь нашей компанией.");
+    }
+}
